6dc176481b7d5d6e449d0b7e1065ee392aaaa960
A Q Khan <aqk.neo@gmail.com>
2020-04-21 17:38:11 +0500

    Basic setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    const template = `
+    <header>
+        <h1>JS component, Header woohoo!</h1>
+        <p>Some saaein bacha</p>
+    </header>
+    `
+    return template
+}
+
+export default Header;
